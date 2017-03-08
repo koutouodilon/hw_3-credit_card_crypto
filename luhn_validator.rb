ae7ef0583993023a2969f753cbe53fdbf31a8ef7
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-08 18:42:53 +0800

    Code optimized

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,15 +5,7 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    even_digits = []
-    odd_digits = []
-    nums_a.reverse.each.with_index do |val, index|
-      (index.odd?)? odd_digits << val : even_digits << val
-    end
-    double_second_digit = Proc.new do |digit|
-      (digit * 2 > 9)? digit * 2 - 9 : digit * 2
-    end
-    total = odd_digits.map(&double_second_digit).reduce(&:+) + even_digits.reduce(&:+)
-    return (total % 10 == 0)
+    total = nums_a.reverse.map.with_index { |e, i| i.odd? ? ((e * 2) > 9 ? e * 2 - 9 : e * 2) : e }.reduce(&:+)
+    total % 10 == 0
   end
 end
