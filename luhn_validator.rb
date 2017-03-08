61f9cc277acf6eddd68e308a9b9c504425480e7b
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-08 19:29:01 +0800

    Code style improved

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,4 @@
+# LuhnValidator
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,7 +6,13 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    total = nums_a.reverse.map.with_index { |e, i| i.odd? ? ((e * 2) > 9 ? e * 2 - 9 : e * 2) : e }.reduce(&:+)
-    total % 10 == 0
+    total = nums_a.reverse.map.with_index do |e, i|
+      if i.odd?
+        ((e * 2) > 9 ? e * 2 - 9 : e * 2)
+      else
+        e
+      end
+    end.reduce(&:+)
+    (total % 10).zero?
   end
 end
