ce1730a1423eb93bae7b1b89997887a2970de70d
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-09 20:10:32 +0800

    Sum optimized

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -6,13 +6,13 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    total = nums_a.reverse.map.with_index do |e, i|
+    total = nums_a.reverse.map!.with_index do |e, i|
       if i.odd?
         ((e * 2) > 9 ? e * 2 - 9 : e * 2)
       else
         e
       end
-    end.reduce(&:+)
+    end.sum
     (total % 10).zero?
   end
 end
