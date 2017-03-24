c3351982478c0abd75268a161d9d03e2a10d048d
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-03-24 09:46:09 +0800

    Assignment part C

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,17 +1,26 @@
 require 'rbnacl/libsodium'
+require 'base64'
 
+# Modern Symmetric Cipher
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.encode64(key)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.decode64(key))
+    simple_ciphertext = simple_box.encrypt(document)
+    Base64.encode64(simple_ciphertext)
   end
 
-  def self.decrypt(encrypted_cc, key)
+  def self.decrypt(aes_crypt, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.decode64(key))
+    simple_box.decrypt(Base64.decode64(aes_crypt))
   end
 end
--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,10 +1,14 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
+require_relative '../double_trans_cipher'
+require_relative '../sk_cipher'
 require 'minitest/autorun'
+require 'base64'
 
 describe 'Test card info encryption' do
   before do
-    @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
+    @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
+                         'Soumya Ray', 'Visa')
     @key = 3
   end
 
@@ -36,6 +40,58 @@ describe 'Test card info encryption' do
     end
   end
 
-  # TODO: Add tests for double transposition and modern symmetric key ciphers
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
+  describe 'Using Double transposition cipher' do
+    it 'should encrypt card information' do
+      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+      dec = DoubleTranspositionCipher.decrypt(enc, @key)
+      dec.must_equal @cc.to_s
+    end
+  end
+
+  describe 'Using ModernSymmetric Cipher' do
+    it 'should generate new Base64 keys' do
+      key = ModernSymmetricCipher.generate_new_key
+      (key =~ %r{^[A-Za-z0-9+\/]+={0,3}$}).nil?.must_equal false
+    end
+
+    it 'should generate different keys each time' do
+      key1 = ModernSymmetricCipher.generate_new_key
+      key2 = ModernSymmetricCipher.generate_new_key
+      key1.wont_equal key2
+    end
+
+    it 'should encrypt credit card objects' do
+      @key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, @key)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt encrypted credit card objects' do
+      @key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, @key)
+      dec = ModernSymmetricCipher.decrypt(enc, @key)
+      dec.must_equal @cc.to_s
+    end
+
+    it 'should not replay the same encryption twice' do
+      @key = ModernSymmetricCipher.generate_new_key
+      enc1 = ModernSymmetricCipher.encrypt(@cc.to_s, @key)
+      enc2 = ModernSymmetricCipher.encrypt(@cc.to_s, @key)
+      enc1.wont_equal enc2
+    end
+
+    it 'should not yield depth in encryption' do
+      @key = ModernSymmetricCipher.generate_new_key
+      enc1 = ModernSymmetricCipher.encrypt(@cc.to_s, @key)
+      enc2 = ModernSymmetricCipher.encrypt(@cc.to_s, @key)
+      enc1.wont_equal enc2
+    end
+  end
 end
